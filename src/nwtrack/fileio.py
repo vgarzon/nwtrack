c3164b6d5f66569f9d90d1c60b1b9d780d3aaded
victor <vegarzon@yahoo.com>
2025-12-21 18:34:47 -0800

    add function ro write records to csv

--- src/nwtrack/fileio.py
+++ src/nwtrack/fileio.py
@@ -5,19 +5,38 @@ File input / output utility functions.
 import csv
 
 
-def csv_file_to_list_dict(csv_file_path: str) -> list[dict]:
-    """
-    Read a CSV file and return its contents as a list of dictionaries.
-    Each dictionary represents a row in the CSV file, with column headers as keys.
+def csv_to_records(csv_file_path: str) -> list[dict]:
+    """Read records from a CSV file
 
     Args:
         csv_file_path (str): Path to the CSV file.
 
     Returns:
-        list[dict]: List of dictionaries representing the CSV rows.
+        list[dict]: List of records as dictionaries.
     """
     with open(csv_file_path, "r") as file:
         reader = csv.DictReader(file)
         data = [row for row in reader]
 
     return data
+
+
+def records_to_csv(records, csv_file_path, fieldnames=None):
+    """Write records to a CSV file.
+
+    Args:
+        records (list of dict): List of records to write.
+        csv_file_path (str): Path to the output CSV file.
+        fieldnames (list of str): List of field names for the CSV.
+            If None, use first dict keys.  Default: None
+
+    Returns:
+        None
+    """
+    if fieldnames is None and records:
+        fieldnames = list(records[0].keys())
+
+    with open(csv_file_path, mode="w", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(records)
