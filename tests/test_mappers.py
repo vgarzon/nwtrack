8c01b4d6bac5ebe836dd2eacdc2c873b881071f1
victor <vegarzon@yahoo.com>
2025-12-22 19:20:52 -0800

    test: mapper methods

---
+++ tests/test_mappers.py
@@ -0,0 +1,107 @@
+"""
+Test record to entity mappers.
+"""
+
+from nwtrack.mappers import (
+    Mapper,
+    AccountMapper,
+    CategoryMapper,
+    CurrencyMapper,
+    BalanceMapper,
+    ExchangeRateMapper,
+)
+
+from nwtrack.models import (
+    Currency,
+    Category,
+    Account,
+    Balance,
+    ExchangeRate,
+    Month,
+    Status,
+    Side,
+)
+
+
+def test_currency_mapper() -> None:
+    mapper: Mapper = CurrencyMapper()
+    record = {"code": "USD", "description": "US Dollar"}
+    entity = mapper.to_entity(record)
+    assert isinstance(entity, Currency)
+    assert entity.code == "USD"
+    assert entity.description == "US Dollar"
+    record_converted = mapper.to_record(entity)
+    assert record_converted == record
+
+
+def test_category_mapper() -> None:
+    mapper: Mapper = CategoryMapper()
+    record = {"name": "Cash", "side": "asset"}
+    entity = mapper.to_entity(record)
+    assert isinstance(entity, Category)
+    assert entity.name == "Cash"
+    assert isinstance(entity.side, Side)
+    assert str(entity.side) == "asset"
+    record_converted = mapper.to_record(entity)
+    assert record_converted == record
+
+
+def test_account_mapper() -> None:
+    mapper: Mapper = AccountMapper()
+    record = {
+        "id": 1,
+        "name": "Checking",
+        "description": "Main checking account",
+        "category": "Cash",
+        "currency": "USD",
+        "status": "active",
+    }
+    entity = mapper.to_entity(record)
+    assert isinstance(entity, Account)
+    assert entity.id == 1
+    assert entity.name == "Checking"
+    assert entity.description == "Main checking account"
+    assert entity.category_name == "Cash"
+    assert entity.currency_code == "USD"
+    assert isinstance(entity.status, Status)
+    assert str(entity.status) == "active"
+    record_converted = mapper.to_record(entity)
+    assert record_converted == record
+
+
+def test_balance_mapper() -> None:
+    mapper: Mapper = BalanceMapper()
+    record = {
+        "id": 1,
+        "account_id": 1,
+        "month": "2023-05",
+        "amount": 10000,
+    }
+    entity = mapper.to_entity(record)
+    assert isinstance(entity, Balance)
+    assert entity.id == 1
+    assert entity.account_id == 1
+    assert isinstance(entity.month, Month)
+    assert entity.month.year == 2023
+    assert entity.month.month == 5
+    assert entity.amount == 10_000
+    record_converted = mapper.to_record(entity)
+    assert record_converted == record
+
+
+def test_exchange_rate_mapper() -> None:
+    mapper: Mapper = ExchangeRateMapper()
+    record = {
+        "currency": "EUR",
+        "month": "2023-05",
+        "rate": 1.1,
+    }
+    entity = mapper.to_entity(record)
+    assert isinstance(entity, ExchangeRate)
+    assert entity.currency_code == "EUR"
+    assert isinstance(entity.month, Month)
+    assert entity.month.year == 2023
+    assert entity.month.month == 5
+    assert entity.rate == 1.1
+    record_converted = mapper.to_record(entity)
+    assert record_converted == record
