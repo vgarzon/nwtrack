b5bedc8edc3add078c23b64d385972c6bcb0db08
victor <vegarzon@yahoo.com>
2026-01-03 15:32:21 -0800

    org: split use cases into separate submodules

--- src/nwtrack/use_cases/balance_updater.py
+++ src/nwtrack/use_cases/balance_updater.py
@@ -2,70 +2,13 @@
 Use cases for updating account balances and generating reports.
 """
 
-from pathlib import Path
-
-from nwtrack.admin import DBAdminService
-from nwtrack.config import Config
 from nwtrack.container import Container
 from nwtrack.models import Month
 from nwtrack.services import (
     AccountService,
-    InitDataService,
     ReportService,
     UpdateService,
 )
-
-
-class DBInitializerCSV:
-    """Initialize the database from CSV files."""
-
-    def __init__(self, container: Container) -> None:
-        self._container = container
-        self._config: Config = self._container.resolve(Config)
-        self._admin_svc: DBAdminService = self._container.resolve(DBAdminService)
-        self._data_svc: InitDataService = self._container.resolve(InitDataService)
-
-    def run(self, file_paths: dict[str, str]) -> None:
-        print(f"Database file path: {self._config.db_file_path}")
-        print(f"DDL script path: {self._config.db_ddl_path}")
-        print("Specified CSVfile paths:")
-        for key, path in file_paths.items():
-            print(f"  {key}: {path}")
-        # TODO: Use RepoRegistry to specify required keys
-        required_keys = {
-            "accounts",
-            "balances",
-            "categories",
-            "currencies",
-            "exchange_rates",
-        }
-        self._validate_file_path_keys(file_paths, required_keys)
-        self._validate_file_paths(file_paths)
-        print("WARNING: This script will DELETE and RE-CREATE the database.")
-        confirmation = input("Type 'YES' to continue: ")
-        if confirmation != "YES":
-            print("Quitting.")
-            return
-
-        print("Initializing SQLite database.")
-        self._admin_svc.init_database()
-        self._data_svc.insert_data_from_csv(file_paths)
-        print("Database initialization complete.")
-
-    def _validate_file_path_keys(
-        self, file_paths: dict[str, str], required_keys: set[str]
-    ) -> None:
-        print("Validating required file path keys.")
-        missing_keys = required_keys - file_paths.keys()
-        if missing_keys:
-            raise KeyError(f"Missing required file paths for keys: {missing_keys}")
-
-    def _validate_file_paths(self, file_paths: dict[str, str]) -> None:
-        print("Validating file paths.")
-        for key, path in file_paths.items():
-            _path = Path(path)
-            if not _path.is_file():
-                raise FileNotFoundError(f"Path for '{key}' is not a file: {path}")
 
 
 class BalanceUpdater:
