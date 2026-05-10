8c00411dd464cdf9d69fbf7abdb508301346d462
victor <vegarzon@yahoo.com>
2026-05-10 08:33:41 -0700

    Add shared aggregation reporting contracts

--- src/nwtrack/application/ports/reporting.py
+++ src/nwtrack/application/ports/reporting.py
@@ -4,11 +4,21 @@ Reporting related queries protocols
 
 from typing import Protocol
 
-from nwtrack.application.dto import MonthlyCategoryBalance
+from nwtrack.application.dto import (
+    MonthlyCategoryBalance,
+    SingleMonthAggregationRequest,
+    SingleMonthAggregationResult,
+)
 from nwtrack.domain.value_objects import Month
 
 
 class ReportingQueries(Protocol):
+    def aggregate_single_month(
+        self, request: SingleMonthAggregationRequest
+    ) -> SingleMonthAggregationResult:
+        """Group one month of balances by a supported aggregation dimension."""
+        ...
+
     def monthly_balance_total_by_category(
         self, month: Month
     ) -> list[MonthlyCategoryBalance]:
